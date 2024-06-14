6861c8a39c43b1e739522325d9788e5d911d2429
ErenEmre <156677294+ErenEmreK@users.noreply.github.com>
2024-06-14 21:39:14 +0300

    hide func s

--- content.js
+++ content.js
@@ -1,5 +1,29 @@
+var wanted_languages = ['French', 'Rus'];
 
-    
+function checkAndHide(video_element, wanted_langs) {
+    const lang = getLangByDescription(video_element);
+        if (!(wanted_langs.includes(lang))) {
+            video_element.style.display = 'none';
+        }
+}
+
+function observeResults(callback) {
+    const observer = new MutationObserver(mutations => {
+        mutations.forEach(mutation => {
+            mutation.addedNodes.forEach(node => {
+                if (node.nodeType === 1 && node.tagName === 'YTD-VIDEO-RENDERER') {
+                    checkAndHide(node, wanted_languages);
+                }
+            });
+        });
+    });
+
+    observer.observe(document.querySelector('ytd-app'), {
+        childList: true,
+        subtree: true
+    });
+}
+
 function getLangByDescription(video_element) {
     
     function textToLang(title, description) {
@@ -21,26 +45,41 @@ function getLangByDescription(video_element) {
     let snippetElement = video_element.querySelector('.metadata-snippet-text-navigation');
     let description = snippetElement.textContent.trim();
 
-    console.log("Title: ", title);
     language = textToLang(title, description);
     return language;
 }
 
-function hideVideos(wanted_langs) {
-    let videos = document.querySelectorAll('ytd-video-renderer');
-    videos.forEach(video => {
-        const lang = getLangByDescription(video);
-        if (!(lang in wanted_langs)) {
-            video.style.display = 'none';
-        }
-    });
-}
 
 function main() {
+    //TODO check playlist and shorts too
     console.log("Hello from Filter");
-    hideVideos(['French']);
+    document.querySelectorAll('ytd-video-renderer, yt-reel').forEach(video => {
+        checkAndHide(video, wanted_languages);
+    });
+
+    observeResults();
+
 }
 
 window.addEventListener('load', main);
 
-//TODO make it dynamic and block as you slide
+
+
+
+
+
+
+
+
+
+function getVideoIds() {
+    const videoElements = document.querySelectorAll('ytd-video-renderer');
+    const videoIds = [];
+    videoElements.forEach(video => {
+        const videoId = video.querySelector('a#video-title').href.split('v=')[1];
+        if (videoId) {
+            videoIds.push(videoId);
+        }
+    });
+    return videoIds;
+}
